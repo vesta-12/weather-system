0b3151a48b5ce6bf23a9f1d1d4f82026693fb633
vesta-12 <sofiya.serbina@narxoz.kz>
2026-03-31 10:24:27 +0500

    adding readme

---
+++ src/main/java/org/example/weathersystem/WeatherSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.weathersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/weathersystem/WeatherSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.weathersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
